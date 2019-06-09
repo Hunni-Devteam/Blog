9937fe3c143c476508d75044b50931b3bf1df10d
Hunni <hunni.devteam@gmail.com>
2019-06-10 03:48:05 +0900

    Add about page contents with navigation link

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -1,26 +1,26 @@
 <template>
   <nav class="main-navigation" style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
     <div style="color: #f9f9f9; text-align: center; margin-bottom: 56px; margin-top: 48px;">
-      <div @click="$router.push('/')" style="display: inline-block; margin-bottom: 8px; font-size: 30px; letter-spacing: 0.16px; cursor: pointer;">Hunni.Devteam</div><!--
+      <div @click="$router.push('/')" style="display: inline-block; margin-bottom: 8px; font-size: 30px; letter-spacing: -0.36px; cursor: pointer;">Hunni.Devteam</div><!--
    --><br>
       <span class="catchphrase" style="font-size: 17px; color: #d9d9d9;">Own the day, own your life</span>
     </div>
     <ul style="margin-bottom: 56px; font-size: 18px; color: #e9e9e9; width: 100%;">
-      <li style="line-height: 48px; margin: 0px 56px;">
+      <li style="line-height: 48px; margin: 0px 56px;" @click="$router.push('/about')">
         <!-- <font-awesome-icon :icon="['fal', 'user-secret']"></font-awesome-icon> -->
-        About
+        <span>About</span>
       </li>
       <li style="line-height: 48px; margin: 0px 56px;">
-        Development Archive
+        <span>Development Archive</span>
         <ul style="line-height: 28px; font-size: 14px; color: #b9b9b9;">
-          <li>- Web Frontend</li>
-          <li>- Web Backend</li>
-          <li>- API Document</li>
-          <li>- Algorithm</li>
-          <li>- Development Tip</li>
-          <li>- Language Spec</li>
-          <li>- Issues</li>
-          <li>- DevOps</li>
+          <li><span>- Web Frontend</span></li>
+          <li><span>- Web Backend</span></li>
+          <li><span>- API Document</span></li>
+          <li><span>- Algorithm</span></li>
+          <li><span>- Development Tip</span></li>
+          <li><span>- Language Spec</span></li>
+          <li><span>- Issues</span></li>
+          <li><span>- DevOps</span></li>
         </ul>
       </li>
       <li style="line-height: 48px; margin: 0px 56px;">Life Archive</li>
@@ -31,10 +31,13 @@
   </nav>
 </template>
 
-<style type="text/css">
-  .main-navigation {
+<style lang="scss">
+  nav.main-navigation {
     width: 320px;
     background-color: #292929;
+    li {
+      span { cursor: pointer; }
+    }
   }
 </style>
 
--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,259 @@
 <template>
-  <div>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </div>
+  <section id="page-about" class="page-content-wrapper">
+    <!-- breadcrumb -->
+    <div style="height: 48px; line-height: 48px; background-color: #fff; margin: 0px 0px 24px 0px; padding: 0px 24px;">
+      home / about
+    </div>
+    <div class="page-about-content">      
+      <h1>강 훈 (Hun Kang)</h1>
+      <p style="font-size: 15px;">
+        서울에서 3년차 웹 개발자로 일하고 있으며 특히 프론트엔드 기술 숙련도가 높습니다.<br>
+        신규 프로젝트 개발, UI 최적화, 솔루션 리뉴얼 등의 업무를 수행한 경험이 있습니다.<br>
+        주어진 자원과 환경 내에서 최선을 다해 업무를 수행하고, 다른 개발자가 더 효율적으로 일할 수 있도록 돕는 것을 보람차게 여깁니다.
+      </p>
+      <!-- Technical Skills -->
+      <h2 class="subject">Technical Skills Summary</h2>
+      <ul class="depth-1">
+        <li>
+        <h3>Programming Languages</h3>
+        <ul class="depth-2">
+          <li><b>ECMAScript</b> / Typescript / PHP / Visual Basic(for Classic ASP)</li>
+        </ul>
+        </li>
+        <li>
+        <h3>Web Frontend</h3>
+        <ul class="depth-2">
+          <li><b>Vue</b> / Vuex / Angular / Cordova / jQuery / DOM API</li>
+          <li><b>Ionic</b> / <b>Bootstrap</b> / <b>Element UI</b> / Semantic UI</li>
+          <li>SASS(SCSS)</li>
+        </ul>
+        </li>
+        <li>
+        <h3>Server Frameworks</h3>
+        <ul class="depth-2">
+          <li><b>Laravel</b></li>
+        </ul>
+        </li>
+        <li>
+        <h3>Database</h3>
+        <ul class="depth-2">
+          <li>MS-SQL / MySQL</li>
+        </ul>
+        </li>
+        <li>
+        <h3>Design Tools</h3>
+        <ul class="depth-2">
+          <li>Adobe Photoshop</li>
+          <li>Adobe Illustrator</li>
+        </ul>
+        </li>
+      </ul>
+      <!-- Technical Experiences -->
+      <h2 class="subject">Technical Experiences</h2>
+      <ul class="depth-1">
+        <li>
+          <h3>Sellmate Ltd. - Software Developer (2016.08.17 ~ currently working)</h3>
+
+          '쇼핑몰 통합 솔루션'을 운영하는 셀메이트는, 다양한 판매처의 주문을 자동으로 통합해<br>
+          재고할당 - 송장출력 - 발송으로 이어지는 주문 처리과정을 한 번에 처리할 수 있도록 돕고<br>
+          주문의 C/S관리 및 재고관리, 통계 기능을 제공하여 쇼핑몰 운영, 관리 부담을 획기적으로 개선해주는 '셀메이트'를 기점으로<br>
+          오프라인 주문 판매 및 관리를 돕는 Sellmate POS, C/S 관리 효율을 높이는 SMS/LMS/알림톡 발송 서비스 등을 제공합니다.
+
+          <h4>1. 회사 소개 (www.sellmate.co.kr) 사이트 리뉴얼 (2016.08 ~ 2016.11)</h4>
+          <ul class="depth-2">
+            <li>UI 개선 및 상세 페이지 <b>디자인 시스템 통일</b> (표준 폰트 정의 / 사이즈 통일 / 일관성 향상)</li>
+            <li>Table-Based Layout => <b>Bootstrap 3 기반 재작성</b> (반응형 웹 디자인 도입 / 성능 개선)</li>
+            <li><b>Google Analytics 연동</b> - 사용자 흐름 모니터링, 사이트 검색 엔진 최적화(SEO) 작업</li>
+          </ul>
+          <h4>2. 사내 고객사 관리 시스템 리뉴얼 (2016.11 ~ 2017.01)</h4>
+          <ul class="depth-2">
+            <li>MVC 패턴 기반 리팩토링</li>
+            <li>Admin Theme 전체 디자인 및 Vue-Based SPA로 재작성</li>
+          </ul>
+          <!-- <h4>3. Goalian - 목표 관리 애플리케이션 개발 (2016.11 ~ 2017.03)</h4>
+          <ul class="depth-2">
+            <li>jQuery + Boostrap 기개발된 앱 Vue / Laravel 리뉴얼</li>
+          </ul> -->
+          <h4>3. 셀메이트 메시지 발송 시스템 리뉴얼 (2017.04 ~ 2017.06)</h4>
+          <ul class="depth-2">
+            <li>Vue SPA로 재작성, Inspinia Admin Theme 적용</li>
+          </ul>
+          <h4>4. 셀메이트 솔루션 UI 리뉴얼 - 28 / 149 Pages, Popups (2018.03 ~ 2018.06)</h4>
+          <ul class="depth-2">
+            <li>
+              <h4>기술 스택</h4>
+              Classic ASP / Bootstrap 3 / jQuery
+            </li>
+            <li>
+              <h4>주요 작업</h4>
+              <b>프로토타입 디자인</b><br>
+              <b>대시보드, 상단 메뉴, 좌측 메뉴, 하단 메뉴</b> 재작성<br>
+              호환성 보기(IE5 기준 렌더링) 기준으로 작성된 프론트엔드 코드 => <b>Bootstrap 3 기반 재작성</b><br>
+              <b>크로스 브라우징 / DOM API 호환성 테스트</b> 및 오류 해결 자문
+            </li>
+          </ul>
+          <h4>5. 네이버 스마트스토어 API 주문 연동 시스템 개발 (2018.07)</h4>
+          <ul class="depth-2">
+            <li>
+              <h4>기술 스택</h4>
+              Classic ASP / Python
+            </li>
+            <li>
+              <h4>주요 작업</h4>
+              Classic ASP - MSXML2.ServerXMLHTTP 라이브러리를 사용한 <b>SOAP 기반 Server-To-Server API 연동</b><br>
+              API 요청결과 검수 문서 작성 및 Production 라이센스 발급<br>
+              Cron Application 연동에 필요한 Python 스크립트 작성
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <!-- Projects -->
+      <h2 class="subject">Projects</h2>
+      <ul class="depth-1">
+        <li>
+        <h3>Bomm - 사진 공유, 모임 관리 앱 (2017.06 ~ current)</h3>
+        <p>각종 모임의 추억거리를 사진으로 묶어 업로드하고, 참석자들에게 공유할 수 있는 클라우드 앨범 서비스입니다.</p>
+        <ul class="depth-2">
+          <li>
+          <h4>Role</h4>
+          클라이언트 구현 / 서버 API 구현 / 데이터베이스 설계 / 스토어 배포 총괄
+          </li>
+          <li>
+          <h4>Links</h4>
+          Repository : Private
+          <br>
+          Android : <a href="https://play.google.com/store/apps/details?id=kr.co.bomm" target="_blank">https://play.google.com/store/apps/details?id=kr.co.bomm</a>
+          <br>
+          iOS : <a href="https://itunes.apple.com/kr/app/%EB%B4%84-%EB%AA%A8%EC%9E%84-%EA%B4%80%EB%A6%AC-%EC%B6%94%EC%96%B5-%EC%8C%93%EA%B8%B0/id1345709778?mt=8" target="_blank">
+            https://itunes.apple.com/kr/app/%EB%B4%84-%EB%AA%A8%EC%9E%84-%EA%B4%80%EB%A6%AC-%EC%B6%94%EC%96%B5-%EC%8C%93%EA%B8%B0/id1345709778?mt=8
+          </a>
+          </li>
+          <li>
+          <h4>기술 스택</h4>
+          <h4>Frontend</h4>
+          Angular, Ionic, Cordova Framework
+          <h4>Backend</h4>
+          Laravel, MySQL, AWS EC2, AWS S3
+          </li>
+          <li>
+          <h4>주요 기능</h4>        
+          모임 생성 및 참석시간 미리 알림<br>
+          연락처, SNS로 모임 참여 링크 공유<br>
+          사진 업로드, 다운로드, 공유 및 댓글 달기<br>
+          친구 추가, 친구와 같이 한 모임 목록 보기<br>
+          마음에 드는 사진 즐겨 찾기에 추가<br>
+          </li>
+        </ul>
+        </li>
+        <li>
+        <h3>국민대학교 동아리연합회 "Sketch" (2019.02 ~ current)</h3>
+        <p>
+          국민대학교 동아리 관리 시스템입니다.<br>
+          공지, 각종 양식 공유, 시설 수리신청 및 공용공간 일정 관리/신청 기능을 제공합니다.
+        </p>
+        <ul class="depth-2">
+          <li>
+          <h4>Role</h4>
+          프론트엔드 개발 총괄
+          </li>
+          <li>
+          <h4>Links</h4>
+          Repository : <a href="https://github.com/Hunni-Devteam/KMU_club_manager_client" target="_blank">https://github.com/Hunni-Devteam/KMU_club_manager_client</a>
+          <br>
+          Web(Desktop) : <a href="http://www.kmudongari.com/" target="_blank">http://www.kmudongari.com/</a>
+          <br>
+          </li>
+          <li>
+          <h4>기술 스택</h4>
+          <h4>Frontend</h4>
+          Vue, Vuex, Element UI
+          <h4>Backend</h4>
+          Spring boot 2.0, MySQL, AWS EC2
+          </li>
+        </ul>
+        </li>
+      </ul>
+      <!-- Education -->
+      <h2 class="subject">Education</h2>
+      <ul class="depth-1">
+        <li>
+        <h3>세명컴퓨터고등학교 - Semyung Computer High School (2015 - 2017)</h3>
+        <ul class="depth-2">
+          <li>
+          <h4>스마트콘텐츠과 (Department of Smart Contents) 졸업</h4>
+          </li>
+        </ul>
+        </li>
+      </ul>
+      <!-- Activities -->
+      <h2 class="subject">Activities</h2>
+      <ul class="depth-1">
+        <li>
+        <h3>세명컴퓨터고등학교 - Semyung Computer High School</h3>
+        <ul class="depth-2">
+          <li><h4>기능경기대회 준비반(기능인재반) 웹 디자인 및 개발 과정 (2014-2015)</h4></li>
+          <li><h4>글로벌숙련기술진흥원[GIFTS] 숙련기술전수과정 - 모던 웹 디자인 부문 우수 수료자(2016)</h4></li>
+        </ul>
+        </li>
+      </ul>
+      <!-- Certificates -->
+      <h2 class="subject">Certificates</h2>
+      <ul class="depth-1">
+        <li>
+        <h3>한국산업인력공단</h3>
+        <ul class="depth-2">
+          <li><h4>웹디자인기능사 (Craftsman Web Design)</h4></li>
+          <li><h4>정보처리기능사 (Craftsman Information Processing)</h4></li>
+          <li><h4>컴퓨터그래픽스운용기능사 (Craftsman Computer Graphics Operation)</h4></li>
+        </ul>
+        </li>
+        <!-- <li><h4>워드프로세서</h4></li>
+        <li><h4>리눅스마스터 2급</h4></li> -->
+      </ul>
+    </div>
+  </section>
 </template>
+
+<style lang="scss">
+  #page-about {
+    .page-about-content {
+      line-height: 1.4;
+      font-size: 15px;
+      background: #fff;
+      margin: 24px;
+      padding: 32px;
+      padding-top: 24px;
+    }
+    h1,h2,h3,h4,h5,h6 { font-weight: normal; }
+    h4 { margin: 0; line-height: 40px; font-size: 15px; }
+    ul, ol {
+      list-style: disc;
+      padding-left: 30px;
+    }
+    body { font-size: 16px; }
+    /*ul { list-style: none }*/
+    .subject {
+      // border-left: 4px solid #42b983;
+      text-indent: 12px;
+      font-size: 18px;
+      height: 48px; line-height: 48px;
+      color: #222;
+      background-color: #f9f9f9;
+    }
+    .depth-1 {
+      color: #4c4c4c;
+      .depth-2 {
+        & { list-style: circle; }
+        li {
+          h4 { color: #4c4c4c; }
+          line-height: 24px; color: #777;
+          b, strong { color: #3c3c3c; }
+        }
+      }
+    }
+  }
+</style>
+
+<script type="text/javascript">
+  export default {}
+</script>

681784c1d845cb3ff82586fcd59131de47095258
강훈 <hunni.sellmate.co.kr>
2019-06-10 10:52:59 +0900

    Optimize readability by font color

--- pages/about.vue
+++ pages/about.vue
@@ -17,21 +17,21 @@
         <li>
         <h3>Programming Languages</h3>
         <ul class="depth-2">
-          <li><b>ECMAScript</b> / Typescript / PHP / Visual Basic(for Classic ASP)</li>
+          <li><strong>ECMAScript</strong> / Typescript / PHP / Visual Basic(for Classic ASP)</li>
         </ul>
         </li>
         <li>
         <h3>Web Frontend</h3>
         <ul class="depth-2">
-          <li><b>Vue</b> / Vuex / Angular / Cordova / jQuery / DOM API</li>
-          <li><b>Ionic</b> / <b>Bootstrap</b> / <b>Element UI</b> / Semantic UI</li>
+          <li><strong>Vue</strong> / Vuex / Angular / Cordova / jQuery / DOM API</li>
+          <li><strong>Ionic</strong> / <strong>Bootstrap</strong> / <strong>Element UI</strong> / Semantic UI</li>
           <li>SASS(SCSS)</li>
         </ul>
         </li>
         <li>
         <h3>Server Frameworks</h3>
         <ul class="depth-2">
-          <li><b>Laravel</b></li>
+          <li><strong>Laravel</strong></li>
         </ul>
         </li>
         <li>
@@ -54,21 +54,20 @@
         <li>
           <h3>Sellmate Ltd. - Software Developer (2016.08.17 ~ currently working)</h3>
 
-          '쇼핑몰 통합 솔루션'을 운영하는 셀메이트는, 다양한 판매처의 주문을 자동으로 통합해<br>
-          재고할당 - 송장출력 - 발송으로 이어지는 주문 처리과정을 한 번에 처리할 수 있도록 돕고<br>
-          주문의 C/S관리 및 재고관리, 통계 기능을 제공하여 쇼핑몰 운영, 관리 부담을 획기적으로 개선해주는 '셀메이트'를 기점으로<br>
-          오프라인 주문 판매 및 관리를 돕는 Sellmate POS, C/S 관리 효율을 높이는 SMS/LMS/알림톡 발송 서비스 등을 제공합니다.
+          &nbsp;셀메이트는, 다양한 판매처의 주문을 자동으로 통합해 재고할당 - 송장출력 - 발송으로 이어지는 주문 처리과정을 한 번에 처리할 수 있도록 도우며<br>
+          주문의 C/S관리 및 재고관리, 통계 기능을 제공하여 쇼핑몰 운영, 관리 부담을 획기적으로 개선해주는 웹 솔루션인 '셀메이트'의 개발사입니다.<br>
+          이 외에도 셀메이트에 연동해 사용할 수 있는 오프라인 주문 판매 및 관리 솔루션 Sellmate POS, C/S 관리 효율을 높이는 SMS/LMS/알림톡 발송 서비스 등을 제공합니다.
 
           <h4>1. 회사 소개 (www.sellmate.co.kr) 사이트 리뉴얼 (2016.08 ~ 2016.11)</h4>
           <ul class="depth-2">
-            <li>UI 개선 및 상세 페이지 <b>디자인 시스템 통일</b> (표준 폰트 정의 / 사이즈 통일 / 일관성 향상)</li>
-            <li>Table-Based Layout => <b>Bootstrap 3 기반 재작성</b> (반응형 웹 디자인 도입 / 성능 개선)</li>
-            <li><b>Google Analytics 연동</b> - 사용자 흐름 모니터링, 사이트 검색 엔진 최적화(SEO) 작업</li>
+            <li>UI 개선 및 상세 페이지 <strong>디자인 시스템 통일</strong> (표준 폰트 정의 / 사이즈 통일 / 일관성 향상)</li>
+            <li>Table-Based Layout => <strong>Bootstrap 3 기반 재작성</strong> (반응형 웹 디자인 도입 / 성능 개선)</li>
+            <li><strong>Google Analytics 연동</strong> - 사용자 흐름 모니터링, 사이트 검색 엔진 최적화(SEO) 작업</li>
           </ul>
           <h4>2. 사내 고객사 관리 시스템 리뉴얼 (2016.11 ~ 2017.01)</h4>
           <ul class="depth-2">
             <li>MVC 패턴 기반 리팩토링</li>
-            <li>Admin Theme 전체 디자인 및 Vue-Based SPA로 재작성</li>
+            <li>Admin Theme 전체 디자인 및 <strong>Vue.js 기반의 SPA로 재작성</strong></li>
           </ul>
           <!-- <h4>3. Goalian - 목표 관리 애플리케이션 개발 (2016.11 ~ 2017.03)</h4>
           <ul class="depth-2">
@@ -76,7 +75,7 @@
           </ul> -->
           <h4>3. 셀메이트 메시지 발송 시스템 리뉴얼 (2017.04 ~ 2017.06)</h4>
           <ul class="depth-2">
-            <li>Vue SPA로 재작성, Inspinia Admin Theme 적용</li>
+            <li><strong>Vue.js 기반의 SPA로 재작성</strong>, Inspinia Admin Theme(Bootstrap Theme) 적용</li>
           </ul>
           <h4>4. 셀메이트 솔루션 UI 리뉴얼 - 28 / 149 Pages, Popups (2018.03 ~ 2018.06)</h4>
           <ul class="depth-2">
@@ -86,10 +85,10 @@
             </li>
             <li>
               <h4>주요 작업</h4>
-              <b>프로토타입 디자인</b><br>
-              <b>대시보드, 상단 메뉴, 좌측 메뉴, 하단 메뉴</b> 재작성<br>
-              호환성 보기(IE5 기준 렌더링) 기준으로 작성된 프론트엔드 코드 => <b>Bootstrap 3 기반 재작성</b><br>
-              <b>크로스 브라우징 / DOM API 호환성 테스트</b> 및 오류 해결 자문
+              <strong>프로토타입 디자인</strong><br>
+              <strong>대시보드, 상단 메뉴, 좌측 메뉴, 하단 메뉴</strong> 재작성<br>
+              호환성 보기(IE5 기준 렌더링) 기준으로 작성된 프론트엔드 코드 => <strong>Bootstrap 3 기반 재작성</strong><br>
+              <strong>크로스 브라우징 / DOM API 호환성 테스트</strong> 및 오류 해결 자문
             </li>
           </ul>
           <h4>5. 네이버 스마트스토어 API 주문 연동 시스템 개발 (2018.07)</h4>
@@ -100,7 +99,7 @@
             </li>
             <li>
               <h4>주요 작업</h4>
-              Classic ASP - MSXML2.ServerXMLHTTP 라이브러리를 사용한 <b>SOAP 기반 Server-To-Server API 연동</b><br>
+              Classic ASP - MSXML2.ServerXMLHTTP 라이브러리를 사용한 <strong>SOAP 기반 Server-To-Server API 연동</strong><br>
               API 요청결과 검수 문서 작성 및 Production 라이센스 발급<br>
               Cron Application 연동에 필요한 Python 스크립트 작성
             </li>
@@ -177,7 +176,7 @@
       <h2 class="subject">Education</h2>
       <ul class="depth-1">
         <li>
-        <h3>세명컴퓨터고등학교 - Semyung Computer High School (2015 - 2017)</h3>
+        <h3>세명컴퓨터고등학교 - Semyeong Computer High School (2015 - 2017)</h3>
         <ul class="depth-2">
           <li>
           <h4>스마트콘텐츠과 (Department of Smart Contents) 졸업</h4>
@@ -189,7 +188,7 @@
       <h2 class="subject">Activities</h2>
       <ul class="depth-1">
         <li>
-        <h3>세명컴퓨터고등학교 - Semyung Computer High School</h3>
+        <h3>세명컴퓨터고등학교 - Semyeong Computer High School</h3>
         <ul class="depth-2">
           <li><h4>기능경기대회 준비반(기능인재반) 웹 디자인 및 개발 과정 (2014-2015)</h4></li>
           <li><h4>글로벌숙련기술진흥원[GIFTS] 숙련기술전수과정 - 모던 웹 디자인 부문 우수 수료자(2016)</h4></li>
@@ -218,7 +217,7 @@
   #page-about {
     .page-about-content {
       line-height: 1.4;
-      font-size: 15px;
+      font-size: 14px;
       background: #fff;
       margin: 24px;
       padding: 32px;
@@ -234,6 +233,7 @@
     /*ul { list-style: none }*/
     .subject {
       // border-left: 4px solid #42b983;
+      font-weight: bold;
       text-indent: 12px;
       font-size: 18px;
       height: 48px; line-height: 48px;
@@ -241,13 +241,15 @@
       background-color: #f9f9f9;
     }
     .depth-1 {
-      color: #4c4c4c;
+      color: #3c3c3c;
       .depth-2 {
         & { list-style: circle; }
         li {
-          h4 { color: #4c4c4c; }
-          line-height: 24px; color: #777;
-          b, strong { color: #3c3c3c; }
+          h4 { color: #3c3c3c; }
+          line-height: 24px; color: #555;
+          strong {
+          	color: #111;
+          }
         }
       }
     }

8a1a58a5b7a93988d33d08615510dfbdeccdcae1
Hunni <hunni.devteam@gmail.com>
2019-07-07 16:28:35 +0900

    Add page shortcuts

--- assets/pageShortcuts.js
+++ assets/pageShortcuts.js
@@ -5,5 +5,37 @@ export default [
     description: 'Disqus 연동 테스트',
     tags: ['disqus', 'development'],
     src: '/archive/development/disqus-comment-test'
+  },
+  {
+    created_at : '2019-05-23 21:30:00',
+    title: 'Laravel 쿼리 빌더 사용으로 인한 MSSQL 성능 이슈',
+    description: '컬럼 타입 강제 캐스팅으로 인해 인덱스를 타지 않았다.',
+    tags: ['web-backend', 'laravel', 'perfomance', 'issue'],
+    src: '/archive/development/laravel-query-builder-causes-mssql-perfomance-issue',
+    image: '/laravel-logo.png'
+  },
+  {
+    created_at : '2019-05-24 18:00:00',
+    title: 'Cordova-Based SPA에 핫 리로딩 적용하기',
+    description: 'config.xml - content 태그 값 변경으로 손쉽게 적용할 수 있어요.',
+    tags: ['web-frontend', 'cordova', 'vue', 'development-tip'],
+    src: '/archive/development/usage-of-cordova-remote-page-content',
+    image: '/laravel-logo.png'
+  },
+  {
+    created_at : '2019-05-24 18:00:00',
+    title: 'Cordova + Vue.js 연동하기',
+    description: 'Vue CLI 3 너무 편해요!',
+    tags: ['web-frontend', 'cordova', 'vue', 'development-tip'],
+    src: '/archive/development/use-cordova-with-vue-cli-3',
+    image: '/laravel-logo.png'
+  },
+  {
+    created_at : '2019-07-07 15:40:00',
+    title: 'Vuex, 커링, 클로저.',
+    description: '알게 모르게 써오던 개념 정리',
+    tags: ['web-frontend', 'vue', 'development-tip', 'ES6', 'functional-programming'],
+    src: '/archive/development/vuex-currying-closure',
+    image: '/laravel-logo.png'
   }
 ]

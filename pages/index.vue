0f470881d6b1d10c6c1cd3f800f79efbd9d940d1
강훈 <hunni.sellmate.co.kr>
2019-07-23 15:06:41 +0900

    Add draft article filter by first tag name

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,13 @@
 <template>
   <div class="page-content-wrapper">
-    <div class="bg-white" style="margin: 24px;">
-      <h1 style="font-weight: normal;">Recently Posted</h1>
-      <hr>
-      <div @click="$router.push(pageShortcut.src)" v-for="pageShortcut in $store.getters.pageShortcuts" :key="pageShortcut.created_at" style="cursor: pointer; font-size: 16px; padding: 16px;">
-        <div style="display: flex;">          
-          <div>
-            <div style="margin-bottom: 4px;"><span style="font-size: 24px;">{{ pageShortcut.title }}</span></div>
-            {{ pageShortcut.description }}
-          </div>
-          <div style="flex: 1"></div>
-          <div>
-            {{ pageShortcut.created_at }}
-          </div>
-        </div>
+    <div class="bg-white">
+      <div v-if="pageShortcut.tags[0] != 'draft'" @click="$router.push(pageShortcut.src)" v-for="pageShortcut in $store.getters.pageShortcuts" :key="pageShortcut.title" style="display: inline-block; width: 25%; cursor: pointer; font-size: 16px; padding: 16px; vertical-align: top;">
+        <div :style="{ 'background-image': 'url('+pageShortcut.image+')' }" style="height: 154px; background-size: cover; background-position: center;"></div>
+        <div style="margin-bottom: 4px;"><span style="font-size: 16px;">{{ pageShortcut.title }}</span></div>
+        <div style="font-size: 14px;">{{ pageShortcut.description }}</div>
+        <div>{{ $moment(pageShortcut.created_at).format('YYYY-MM-DD') }}</div>
         <div style="margin-top: 8px;">
-          <span style="padding: 3px 6px; margin: 2px; background-color: #6c6c6c; color: #fff; font-size: 13px; border-radius: 3px;" v-for="tag in pageShortcut.tags" :key="tag">{{ tag }}</span>
+          <span style="display: inline-block; padding: 3px 6px; margin: 2px; background-color: #6c6c6c; color: #fff; font-size: 13px; border-radius: 3px;" v-for="tag in pageShortcut.tags" :key="tag">{{ tag }}</span>
         </div>
       </div>
     </div>
